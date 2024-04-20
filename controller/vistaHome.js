a5ebd7b150bf50aa119103195bc0b7010c9401de
Camila Serantoni <camila.serantoni@blue.cl>
2024-04-20 12:28:15 -0400

    fixing extension y eliminado de elementos no funcionales  Co-authored-by:  CrisContrerasC <cristina.contreras.celedon@gmail.com> Co-authored-by: ldevaudl <ldevaudl@gmail.com>

--- controller/vistaHome.js
+++ controller/vistaHome.js
@@ -4,22 +4,6 @@ export const vistaHome = ( req , res) => {
     res.render("home", {
         layout:"main",
         title : "Bienvenidos a Node Express y Handlebars 2024",
-        dataCard : [
-            {
-                title:"Este es el titulo uno",
-                description:"Lorem Ipsum is simply dummy text of the printing and typesetting industry",
-                url:"https://placehold.jp/300x200.png"
-            },
-            {
-                title:"Este es el titulo dos",
-                description:"Lorem Ipsum is simply dummy text of the printing and typesetting industry",
-                url:"https://placehold.jp/300x200.png"
-            },
-            {
-                title:"Este es el titulo tres",
-                description:"Lorem Ipsum is simply dummy text of the printing and typesetting industry",
-                url:"https://placehold.jp/300x200.png"
-            }
-        ]
+        message : "Hola Mundo",
     })
 }
